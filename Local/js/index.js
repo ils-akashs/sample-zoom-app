91c8372a049297b86d87b6d01546338e8b1e1ada
Akash Sawant <sawantakash321@gmail.com>
2019-07-18 18:02:48 +0530

    initial commit

---
+++ Local/js/index.js
@@ -0,0 +1,67 @@
+import {ZoomMtg} from 'zoomus-jssdk';
+
+console.log('checkSystemRequirements');
+console.log(JSON.stringify(ZoomMtg.checkSystemRequirements()));
+
+// it's option if you want to chenge the jssdk dependency link resources.
+// ZoomMtg.setZoomJSLib('https://dmogdx0jrul3u.cloudfront.net/1.4.2/lib', '/av'); // CDN version default
+ZoomMtg.setZoomJSLib('http://localhost:9999/node_modules/zoomus-jssdk/dist/lib', '/av'); // Local version default
+ZoomMtg.preLoadWasm();
+ZoomMtg.prepareJssdk();
+
+const API_KEY = 'jmP6DqemQjay1XcJpll1vg';
+
+/**
+ * NEVER PUT YOUR ACTUAL API SECRET IN CLIENT SIDE CODE, THIS IS JUST FOR QUICK PROTOTYPING
+ * The below generateSignature should be done server side as not to expose your api secret in public
+ * You can find an eaxmple in here: https://marketplace.zoom.us/docs/sdk/native-sdks/Web-Client-SDK/tutorial/generate-signature
+ */
+const API_SECRET = 'zmqEqGdjst5mL4dDYRctq1ZIbQWULQF9SoO7';
+
+document.getElementById('join_meeting').addEventListener('click', e => {
+  e.preventDefault();
+
+  const meetConfig = {
+    apiKey: API_KEY,
+    apiSecret: API_SECRET,
+    meetingNumber: parseInt(document.getElementById('meeting_number').value, 10),
+    userName: document.getElementById('display_name').value,
+    passWord: '',
+    leaveUrl: 'https://zoom.us',
+    role: 0,
+  };
+
+  ZoomMtg.generateSignature({
+    meetingNumber: meetConfig.meetingNumber,
+    apiKey: meetConfig.apiKey,
+    apiSecret: meetConfig.apiSecret,
+    role: meetConfig.role,
+    success(res) {
+      console.log('signature', res.result);
+      ZoomMtg.init({
+        leaveUrl: 'http://www.zoom.us',
+        isSupportAV: true,
+        success() {
+          ZoomMtg.join({
+            meetingNumber: meetConfig.meetingNumber,
+            userName: meetConfig.userName,
+            signature: res.result,
+            apiKey: meetConfig.apiKey,
+            userEmail: 'email@gmail.com',
+            passWord: meetConfig.passWord,
+            success() {
+              $('#nav-tool').hide();
+              console.log('join meeting success');
+            },
+            error(res) {
+              console.log(res);
+            },
+          });
+        },
+        error(res) {
+          console.log(res);
+        },
+      });
+    },
+  });
+});
---
+++ README.md
@@ -0,0 +1,24 @@
+# Web Client SDK
+
+### sample web app (local version)
+
+```javascript
+git clone https://github.com/zoom/sample-app-web.git
+cd sample-app-web/Local
+npm install
+npm run start
+```
+
+open browser http://localhost:9999
+
+### run demo with https
+
+we provide a https option, other machines can join the demo and test audio and video feature.
+
+notice: the certification signed by localhost. don't use in your production.
+
+```
+npm run https
+```
+
+open browser https://localhost:9999
